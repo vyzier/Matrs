462d6e197326c306bf1524e36c98bfeb8c2009b6
Mark Malabanan <mark.malabanan24@gmail.com>
2017-11-12 13:34:03 -0500

    Initial commit

---
+++ Assets/Scripts/CognitoManager.cs
@@ -0,0 +1,124 @@
+﻿//
+// Copyright 2014-2015 Amazon.com, 
+// Inc. or its affiliates. All Rights Reserved.
+// 
+// Licensed under the AWS Mobile SDK For Unity 
+// Sample Application License Agreement (the "License"). 
+// You may not use this file except in compliance with the 
+// License. A copy of the License is located 
+// in the "license" file accompanying this file. This file is 
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, express or implied. See the License 
+// for the specific language governing permissions and 
+// limitations under the License.
+//
+
+//Add the Facebook Unity SDK and uncomment this to enable Facebook login
+#define USE_FACEBOOK_LOGIN
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Amazon;
+using Amazon.CognitoSync;
+using Amazon.Runtime;
+using Amazon.CognitoIdentity;
+using Amazon.CognitoIdentity.Model;
+using Amazon.CognitoSync.SyncManager;
+using Facebook.Unity;
+
+namespace AWSSDK.Examples
+{
+
+    public class CognitoManager : MonoBehaviour
+    {
+
+        public string IdentityPoolId = "";
+
+        public string Region = RegionEndpoint.USEast1.SystemName;
+
+        private RegionEndpoint _Region
+        {
+            get { return RegionEndpoint.GetBySystemName(Region); }
+        }
+
+        private CognitoAWSCredentials _credentials;
+
+        private CognitoAWSCredentials Credentials
+        {
+            get
+            {
+                if (_credentials == null)
+                    _credentials = new CognitoAWSCredentials(IdentityPoolId, _Region);
+                return _credentials;
+            }
+        }
+
+
+        private CognitoSyncManager _syncManager;
+
+        private CognitoSyncManager SyncManager
+        {
+            get
+            {
+                if (_syncManager == null)
+                {
+                    _syncManager = new CognitoSyncManager(Credentials, new AmazonCognitoSyncConfig { RegionEndpoint = _Region });
+                }
+                return _syncManager;
+            }
+        }
+
+        void Start()
+        {
+            UnityInitializer.AttachToGameObject(this.gameObject);
+        }
+
+    #region Public Authentication Providers
+#if USE_FACEBOOK_LOGIN
+        private void FacebookLoginCallback(IResult result)
+        {
+            Debug.Log("FB.Login completed");
+
+            if (result.Error != null || !FB.IsLoggedIn)
+            {
+                Debug.LogError(result.Error);
+            }
+            else
+            {
+                Debug.Log(result.ToString());
+                Credentials.AddLogin ("graph.facebook.com", FB.ClientToken);
+            }
+        }
+
+#endif
+    #endregion
+
+        public void FacebookLogin()
+        {
+#if USE_FACEBOOK_LOGIN
+            if (!FB.IsInitialized)
+            {
+                FB.Init(delegate ()
+                {
+                    Debug.Log("starting thread");
+
+                    // shows to connect the current identityid or create a new identityid with facebook authentication
+                    FB.LogInWithReadPermissions(new List<string>() { "email" }, FacebookLoginCallback);
+                });
+            }
+            else
+            {
+                FB.LogInWithReadPermissions(new List<string>() { "email" }, FacebookLoginCallback);
+            }
+#endif
+        }
+
+        public void AmazonLogin()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PickNumberManager.cs
@@ -0,0 +1,320 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickNumberManager : MonoBehaviour
+{
+
+    public int waterValue;
+    public int foodValue;
+    public int medicationValue;
+    public int firstAidValue;
+    public int toolsValue;
+    public int clothingValue;
+    public int waterCount;
+    public int foodCount;
+    public int medicationCount;
+    public int firstAidCount;
+    public int toolsCount;
+    public int clothingCount;
+
+    public Text waterCount1;
+    public Text foodCount1;
+    public Text medicationCount1;
+    public Text firstAidCount1;
+    public Text toolsCount1;
+    public Text clothingCount1;
+
+    public Text waterCount2;
+    public Text foodCount2;
+    public Text medicationCount2;
+    public Text firstAidCount2;
+    public Text toolsCount2;
+    public Text clothingCount2;
+
+    public Text waterText;
+    public Text foodText;
+    public Text medicationText;
+    public Text firstAidText;
+    public Text toolsText;
+    public Text clothingText;
+
+    public Text waterCO;
+    public Text foodCO;
+    public Text medicationCO;
+    public Text firstAidCO;
+    public Text toolsCO;
+    public Text clothingCO;
+    public Text waterCOText;
+    public Text foodCOText;
+    public Text medicationCOText;
+    public Text firstAidCOText;
+    public Text toolsCOText;
+    public Text clothingCOText;
+    public Image waterImg;
+    public Image foodImg;
+    public Image medicationImg;
+    public Image firstAidImg;
+    public Image toolsImg;
+    public Image clothingImg;
+
+    public Text checkoutTotal;
+
+
+    public void WaterInc()
+    {
+        waterValue++;
+        UpdateUI();
+    }
+
+    public void WaterDec()
+    {
+        if (waterValue > 0)
+            waterValue--;
+
+        UpdateUI();
+    }
+
+    public void FoodInc()
+    {
+        foodValue++;
+        UpdateUI();
+    }
+
+    public void FoodDec()
+    {
+        if (foodValue > 0)
+            foodValue--;
+
+        UpdateUI();
+    }
+
+    public void MedicationInc()
+    {
+        medicationValue++;
+        UpdateUI();
+    }
+
+    public void MedicationDec()
+    {
+        if (medicationValue > 0)
+            medicationValue--;
+
+        UpdateUI();
+    }
+
+    public void FirstAidInc()
+    {
+        firstAidValue++;
+        UpdateUI();
+    }
+
+    public void FirstAidDec()
+    {
+        if (firstAidValue > 0)
+            firstAidValue--;
+
+        UpdateUI();
+    }
+
+    public void ToolsInc()
+    {
+        toolsValue++;
+        UpdateUI();
+    }
+
+    public void ToolsDec()
+    {
+        if (toolsValue > 0)
+            toolsValue--;
+
+        UpdateUI();
+    }
+
+    public void ClothingInc()
+    {
+        clothingValue++;
+        UpdateUI();
+    }
+
+    public void ClothingDec()
+    {
+        if (clothingValue > 0)
+            clothingValue--;
+
+        UpdateUI();
+    }
+
+    public float CalculateWater()
+    {
+        return (0.99f * waterValue);
+    }
+
+    public float CalculateFood()
+    {
+        return (2.5f * foodValue);
+    }
+
+    public float CalculateMedication()
+    {
+        return (3.25f * medicationValue);
+    }
+
+    public float CalculateFirstAid()
+    {
+        return (10.5f * firstAidValue);
+    }
+
+    public float CalculateTools()
+    {
+        return (12.45f * toolsValue);
+    }
+
+    public float CalculateClothing()
+    {
+        return (5.7f * clothingValue);
+    }
+
+    public float CalculatePrice()
+    {
+        return CalculateWater() +
+               CalculateFood() +
+               CalculateMedication() +
+               CalculateFirstAid() +
+               CalculateTools() +
+               CalculateClothing();
+    }
+
+    public void UpdateCheckoutUI()
+    {
+        // Show only the things that were donated
+        waterCOText.text = waterValue.ToString() + " x $0.99";
+        foodCOText.text = foodValue.ToString() + " x $2.50";
+        medicationCOText.text = medicationValue.ToString() + " x $3.25";
+        firstAidCOText.text = firstAidValue.ToString() + " x $10.50";
+        toolsCOText.text = toolsValue.ToString() + " x $12.45";
+        clothingCOText.text = clothingValue.ToString() + " x $5.70";
+
+        if (waterValue <= 0)
+        {
+            waterCO.gameObject.SetActive(false);
+            waterCOText.gameObject.SetActive(false);
+            waterImg.gameObject.SetActive(false);
+        }
+        else
+        {
+            waterCO.gameObject.SetActive(true);
+            waterCOText.gameObject.SetActive(true);
+            waterImg.gameObject.SetActive(true);
+        }
+
+        if (foodValue <= 0)
+        {
+            foodCO.gameObject.SetActive(false);
+            foodCOText.gameObject.SetActive(false);
+            foodImg.gameObject.SetActive(false);
+        }
+        else
+        {
+            foodCO.gameObject.SetActive(true);
+            foodCOText.gameObject.SetActive(true);
+            foodImg.gameObject.SetActive(true);
+        }
+
+        if (medicationValue <= 0)
+        {
+            medicationCO.gameObject.SetActive(false);
+            medicationCOText.gameObject.SetActive(false);
+            medicationImg.gameObject.SetActive(false);
+        }
+        else
+        {
+            medicationCO.gameObject.SetActive(true);
+            medicationCOText.gameObject.SetActive(true);
+            medicationImg.gameObject.SetActive(true);
+        }
+
+        if (firstAidValue <= 0)
+        {
+            firstAidCO.gameObject.SetActive(false);
+            firstAidCOText.gameObject.SetActive(false);
+            firstAidImg.gameObject.SetActive(false);
+        }
+        else
+        {
+            firstAidCO.gameObject.SetActive(true);
+            firstAidCOText.gameObject.SetActive(true);
+            firstAidImg.gameObject.SetActive(true);
+        }
+
+        if (toolsValue <= 0)
+        {
+            toolsCO.gameObject.SetActive(false);
+            toolsCOText.gameObject.SetActive(false);
+            toolsImg.gameObject.SetActive(false);
+        }
+        else
+        {
+            toolsCO.gameObject.SetActive(true);
+            toolsCOText.gameObject.SetActive(true);
+            toolsImg.gameObject.SetActive(true);
+        }
+
+        if (clothingValue <= 0)
+        {
+            clothingCO.gameObject.SetActive(false);
+            clothingCOText.gameObject.SetActive(false);
+            clothingImg.gameObject.SetActive(false);
+        }
+        else
+        {
+            clothingCO.gameObject.SetActive(true);
+            clothingCOText.gameObject.SetActive(true);
+            clothingImg.gameObject.SetActive(true);
+        }
+
+        checkoutTotal.text = "$" + CalculatePrice();
+    }
+
+    public void UpdateAfterDonate()
+    {
+        waterCount -= waterValue;
+        foodCount -= foodValue;
+        medicationCount -= medicationValue;
+        firstAidCount -= firstAidValue;
+        toolsCount -= toolsValue;
+        clothingCount -= clothingValue;
+
+        waterValue = 0;
+        foodValue = 0;
+        medicationValue = 0;
+        firstAidValue = 0;
+        toolsValue = 0;
+        clothingValue = 0;
+
+        waterCount1.text = waterCount.ToString();
+        foodCount1.text = foodCount.ToString();
+        medicationCount1.text = medicationCount.ToString();
+        firstAidCount1.text = firstAidCount.ToString();
+        toolsCount1.text = toolsCount.ToString();
+        clothingCount1.text = clothingCount.ToString();
+    }
+
+    private void UpdateUI()
+    {
+        waterText.text = waterValue.ToString();
+        foodText.text = foodValue.ToString();
+        medicationText.text = medicationValue.ToString();
+        firstAidText.text = firstAidValue.ToString();
+        toolsText.text = toolsValue.ToString();
+        clothingText.text = clothingValue.ToString();
+
+        waterCount2.text = (waterCount - waterValue).ToString();
+        foodCount2.text = (foodCount - foodValue).ToString();
+        medicationCount2.text = (medicationCount - medicationValue).ToString();
+        firstAidCount2.text = (firstAidCount - firstAidValue).ToString();
+        toolsCount2.text = (toolsCount - toolsValue).ToString();
+        clothingCount2.text = (clothingCount - clothingValue).ToString();
+    }
+}
